82aa8b86308f9eb84745164eeba06450b9c2b4d3
beno1234 <benolopesdias@gmail.com>
2025-03-27 10:05:37 -0300

    cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,11 @@
 import express, { type Request, type Response } from "express";
 import { api } from "./api";
+import cors from "cors";
 import session from "cookie-session";
 import { auth } from "./auth";
 
 const app = express();
+app.use(cors());
 app.use(session({ secret: "secret" }));
 app.use(auth);
 app.use(api);
